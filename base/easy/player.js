08d9e412d487c64441638b153f831dedb71b469a
Sergio Garcia <serghyuss@gmail.com>
2021-11-14 22:19:04 -0300

    customizacao-player

--- base/easy/player.js
+++ base/easy/player.js
@@ -8,22 +8,24 @@ window.player = {
     currentPlaying: 0,
     start() {  
         this.update();
-
         this.audio.onended = () => this.next();
     },
     next() {
         this.currentPlaying++;
+        if (this.currentPlaying == this.audioData.length) this.restart();
         this.update();
-        this.audio.play();
     },
     update() {
         this.currentAudio = this.audioData[this.currentPlaying];
-
         this.cover.style.background = `url('${path(
             this.currentAudio.cover)
         }') no-repeat center center / cover`;
         this.title.innerText = this.currentAudio.title;
         this.artist.innerText = this.currentAudio.artist;
         this.audio.src = path(this.currentAudio.file);
+    },
+    restart() {
+        this.currentPlaying = 0;
+        this.update();
     }
-  };
+};
